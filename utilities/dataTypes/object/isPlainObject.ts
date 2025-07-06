d735e88bb1ce48ca0ea5880cd2407e2612e79c6d
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2025-07-06 11:57:49 +0330

    fix: Update isPlainObject examples

--- utilities/dataTypes/object/isPlainObject.ts
+++ utilities/dataTypes/object/isPlainObject.ts
@@ -10,7 +10,7 @@
  * isPlainObject({}); // true
  * isPlainObject([]); // false
  * isPlainObject(null); // false
- * isPlainObject(42); // false
+ * isPlainObject(21); // false
  */
 export const isPlainObject = (value: unknown): value is Record<string, unknown> => 
   typeof value === 'object' && value !== null && !Array.isArray(value);
